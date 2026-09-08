a08214ef515d61dc63684ff0d4eeee187701fb46
Ahmad Mdadeh <eng.ahmadmdadeh@gmail.com>
2026-09-08 15:54:05 +0300

    fix: preserve authentication through signup flow

--- lib/presentation/screens/signup/widgets/signup_form.dart
+++ lib/presentation/screens/signup/widgets/signup_form.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:forsan/core/extension/localization_extension.dart';
 import 'package:forsan/core/resources/app_colors.dart';
 import 'package:forsan/core/resources/app_values.dart';
+import 'package:forsan/core/routes/app_routes.dart';
 import 'package:forsan/presentation/screens/signup/widgets/signup_dropdown_field.dart';
 import 'package:forsan/presentation/widgets/custom_submit_button.dart';
 import 'package:forsan/presentation/widgets/form/custom_input_field.dart';
@@ -128,5 +129,6 @@ class _SignupFormState extends State<SignupForm> {
         _selectedNationality == null) {
       return;
     }
+    const HomeRoute().go(context);
   }
 }
