676c02c9622f5d810a48a4b8f0b8ddf4ddb4e020
Ahmad Mdadeh <eng.ahmadmdadeh@gmail.com>
2026-09-08 12:52:57 +0300

    fix: remove unavailable application routes

--- lib/core/routes/app_shell.dart
+++ lib/core/routes/app_shell.dart
@@ -1,14 +1,11 @@
 import 'package:double_back_to_close_app/double_back_to_close_app.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
-import 'package:tamalok/core/extension/localization_extension.dart';
-import 'package:tamalok/core/resources/app_colors.dart';
-import 'package:tamalok/core/resources/app_values.dart';
-import 'package:tamalok/core/routes/app_routes.dart';
-import 'package:tamalok/presentation/cubit/bottom_bar/bottom_bar_cubit.dart';
-import 'package:tamalok/presentation/widgets/bottom_nav_bar/custom_bottom_nav_bar.dart';
-import 'package:tamalok/presentation/widgets/text/section_title.dart';
+import 'package:forsan/core/extension/localization_extension.dart';
+import 'package:forsan/core/resources/app_colors.dart';
+import 'package:forsan/core/resources/app_values.dart';
+import 'package:forsan/presentation/widgets/bottom_nav_bar/custom_bottom_nav_bar.dart';
+import 'package:forsan/presentation/widgets/text/section_title.dart';
 
 class AppShell extends StatelessWidget {
   const AppShell({required this.navigationShell, super.key});
@@ -17,24 +14,6 @@ class AppShell extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final currentPath = navigationShell.shellRouteContext.routerState.fullPath;
-    final shouldShowBottomBar = <String>{
-      const HomeRoute().location,
-      const FavoritesRoute().location,
-      // const AddPropertyRoute().location,
-      const PropertiesRoute().location,
-      const MoreRoute().location,
-    }.contains(currentPath);
-
-    final bottomBarCubit = context.read<BottomBarCubit>();
-    if (bottomBarCubit.state != shouldShowBottomBar) {
-      WidgetsBinding.instance.addPostFrameCallback((_) {
-        if (!bottomBarCubit.isClosed) {
-          bottomBarCubit.setBottomBarVisibility(shouldShowBottomBar);
-        }
-      });
-    }
-
     return Scaffold(
       extendBody: true,
       body: DoubleBackToCloseApp(
