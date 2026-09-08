ddccb289965549409d73262dcae4be203747443c
Ahmad Mdadeh <eng.ahmadmdadeh@gmail.com>
2026-09-08 16:03:31 +0300

    Navigate to home after signup confirmation

--- lib/presentation/screens/signup/widgets/signup_form.dart
+++ lib/presentation/screens/signup/widgets/signup_form.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:forsan/core/extension/localization_extension.dart';
 import 'package:forsan/core/resources/app_values.dart';
+import 'package:forsan/core/routes/app_routes.dart';
 import 'package:forsan/presentation/screens/signup/widgets/signup_dropdown_field.dart';
 import 'package:forsan/presentation/widgets/custom_submit_button.dart';
 import 'package:forsan/presentation/widgets/form/custom_input_field.dart';
@@ -125,5 +126,7 @@ class _SignupFormState extends State<SignupForm> {
         _selectedNationality == null) {
       return;
     }
+
+    const HomeRoute().go(context);
   }
 }
