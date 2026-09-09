0943c3510dc7ff9e23ef2ac9b330c995fb4e82e4
Ahmad Mdadeh <eng.ahmadmdadeh@gmail.com>
2026-09-09 15:21:01 +0300

    feat: add required order action card

--- lib/presentation/screens/order_details/orders_details_screen.dart
+++ lib/presentation/screens/order_details/orders_details_screen.dart
@@ -8,8 +8,9 @@ import '../../widgets/custom_app_bar.dart';
 import '../../widgets/text/section_title.dart';
 import '../orders/models/order_item.dart';
 import '../orders/widgets/order_card.dart';
-import 'widgets/order_stages_card.dart';
 import 'widgets/order_documents_card.dart';
+import 'widgets/order_required_action_card.dart';
+import 'widgets/order_stages_card.dart';
 import 'widgets/order_summary_card.dart';
 
 class OrdersDetailsScreen extends StatelessWidget {
@@ -54,6 +55,8 @@ class OrdersDetailsScreen extends StatelessWidget {
             const OrderStagesCard(),
             SizedBox(height: AppHeight.h16),
             const OrderDocumentsCard(),
+            SizedBox(height: AppHeight.h16),
+            const OrderRequiredActionCard(),
           ],
         ),
       ),
