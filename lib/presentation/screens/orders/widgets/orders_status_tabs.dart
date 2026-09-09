6e668e3b3286cd6ee294fc1c17b9b4386e912d3c
Ahmad Mdadeh <eng.ahmadmdadeh@gmail.com>
2026-09-09 11:51:28 +0300

    refactor: manage order tabs with cubit

--- lib/presentation/screens/orders/widgets/orders_status_tabs.dart
+++ lib/presentation/screens/orders/widgets/orders_status_tabs.dart
@@ -1,21 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../core/resources/app_colors.dart';
 import '../../../../core/resources/app_fonts.dart';
 import '../../../../core/resources/app_values.dart';
+import '../../../cubit/orders/orders_status_cubit.dart';
 import '../../../widgets/filter_tabs.dart';
 
-class OrdersStatusTabs extends StatefulWidget {
+class OrdersStatusTabs extends StatelessWidget {
   const OrdersStatusTabs({super.key, this.onSelected});
 
   final ValueChanged<int>? onSelected;
 
   @override
-  State<OrdersStatusTabs> createState() => _OrdersStatusTabsState();
+  Widget build(BuildContext context) => BlocProvider(
+    create: (_) => OrdersStatusCubit(),
+    child: _OrdersStatusTabsContent(onSelected: onSelected),
+  );
 }
 
-class _OrdersStatusTabsState extends State<OrdersStatusTabs> {
-  int _selectedIndex = 0;
+class _OrdersStatusTabsContent extends StatelessWidget {
+  const _OrdersStatusTabsContent({this.onSelected});
+
+  final ValueChanged<int>? onSelected;
 
   static const _items = [
     FilterTabItem(label: 'الكل', highlightedText: '(12)'),
@@ -32,18 +39,20 @@ class _OrdersStatusTabsState extends State<OrdersStatusTabs> {
   ];
 
   @override
-  Widget build(BuildContext context) => FilterTabs(
-    items: _items,
-    selectedIndex: _selectedIndex,
-    unselectedTextColor: AppColors.mainText,
-    fontWeight: AppFontWeight.medium,
-    backgroundColor: Colors.transparent,
-    minTabWidth: AppWidth.w98,
-    showTabShadow: true,
-    contentPadding: EdgeInsets.zero,
-    onSelected: (index) {
-      setState(() => _selectedIndex = index);
-      widget.onSelected?.call(index);
-    },
+  Widget build(BuildContext context) => BlocBuilder<OrdersStatusCubit, int>(
+    builder: (context, selectedIndex) => FilterTabs(
+      items: _items,
+      selectedIndex: selectedIndex,
+      unselectedTextColor: AppColors.mainText,
+      fontWeight: AppFontWeight.medium,
+      backgroundColor: Colors.transparent,
+      minTabWidth: AppWidth.w98,
+      showTabShadow: true,
+      contentPadding: EdgeInsets.zero,
+      onSelected: (index) {
+        context.read<OrdersStatusCubit>().selectStatus(index);
+        onSelected?.call(index);
+      },
+    ),
   );
 }
