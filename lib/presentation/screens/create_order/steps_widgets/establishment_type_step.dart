44d7b69e98a90ae178dfd27fd22ecf895c2e4e04
Ahmad Mdadeh <eng.ahmadmdadeh@gmail.com>
2026-09-11 11:15:27 +0300

    refactor: inline applicant role section

--- lib/presentation/screens/create_order/steps_widgets/establishment_type_step.dart
+++ lib/presentation/screens/create_order/steps_widgets/establishment_type_step.dart
@@ -3,7 +3,6 @@ import 'package:forsan/core/extension/localization_extension.dart';
 import 'package:forsan/core/resources/app_colors.dart';
 import 'package:forsan/core/resources/app_fonts.dart';
 import 'package:forsan/core/resources/app_values.dart';
-import 'package:forsan/presentation/screens/create_order/steps_widgets/applicant_role_section.dart';
 import 'package:forsan/presentation/screens/create_order/steps_widgets/establishment_type_card.dart';
 import 'package:forsan/presentation/widgets/text/body_title.dart';
 import 'package:forsan/presentation/widgets/text/section_title.dart';
@@ -58,6 +57,32 @@ class EstablishmentTypeStep extends StatelessWidget {
         icon: Icons.groups_2_outlined,
       ),
     ];
+    final applicantOptions = [
+      (
+        value: 'syrian_citizen',
+        title: context.loc.new_order_syrian_citizen,
+        description: context.loc.new_order_syrian_citizen_description,
+        icon: Icons.account_circle_outlined,
+      ),
+      (
+        value: 'expatriate',
+        title: context.loc.new_order_expatriate,
+        description: context.loc.new_order_expatriate_description,
+        icon: Icons.flight_takeoff_rounded,
+      ),
+      (
+        value: 'foreign_investor',
+        title: context.loc.new_order_foreign_investor,
+        description: context.loc.new_order_foreign_investor_description,
+        icon: Icons.person_add_alt_outlined,
+      ),
+      (
+        value: 'company_representative',
+        title: context.loc.new_order_company_representative,
+        description: context.loc.new_order_company_representative_description,
+        icon: Icons.manage_accounts_outlined,
+      ),
+    ];
 
     return SingleChildScrollView(
       padding: EdgeInsets.fromLTRB(
@@ -106,10 +131,43 @@ class EstablishmentTypeStep extends StatelessWidget {
             SizedBox(height: AppHeight.h12),
           ],
           SizedBox(height: AppHeight.h8),
-          ApplicantRoleSection(
-            selectedValue: selectedApplicantValue,
-            onChanged: onApplicantChanged,
+          Row(
+            children: [
+              Icon(
+                Icons.person_outline_rounded,
+                size: AppSize.s17,
+                color: AppColors.secondary,
+              ),
+              SizedBox(width: AppWidth.w6),
+              Expanded(
+                child: SectionTitle(
+                  text: context.loc.new_order_applicant_role_title,
+                  color: AppColors.primary,
+                  fontSize: AppFontSize.s18,
+                ),
+              ),
+            ],
           ),
+          SizedBox(height: AppHeight.h4),
+          BodyTitle(
+            text: context.loc.new_order_applicant_role_description,
+            color: AppColors.greyText,
+            fontSize: AppFontSize.s13,
+            fontWeight: AppFontWeight.regular,
+            maxLines: 2,
+          ),
+          SizedBox(height: AppHeight.h10),
+          for (final option in applicantOptions) ...[
+            OrderOptionCard(
+              title: option.title,
+              description: option.description,
+              icon: option.icon,
+              height: AppHeight.h75,
+              selected: selectedApplicantValue == option.value,
+              onTap: () => onApplicantChanged(option.value),
+            ),
+            SizedBox(height: AppHeight.h12),
+          ],
         ],
       ),
     );
