de664e33e2e2b222a65acd8377a35685c4475a73
Ahmad Mdadeh <eng.ahmadmdadeh@gmail.com>
2026-09-26 12:47:29 +0300

    Redesign more menu options

--- lib/presentation/screens/more/widgets/more_menu.dart
+++ lib/presentation/screens/more/widgets/more_menu.dart
@@ -17,21 +17,47 @@ class MoreMenu extends StatelessWidget {
     MoreMenuItem(
       icon: Icons.person_outline_rounded,
       title: 'الملف الشخصي',
-      subtitle: 'الملف الشخصي',
+      subtitle: 'عرض وتعديل معلومات حسابك الشخصي',
     ),
     MoreMenuItem(
       icon: Icons.receipt_long_outlined,
       title: 'الفواتير والمدفوعات',
+      subtitle: 'كل المدفوعات والفواتير التي تمت عبر المنصة',
     ),
-    MoreMenuItem(icon: Icons.shield_outlined, title: 'سياسة الخصوصية'),
-    MoreMenuItem(icon: Icons.gavel_outlined, title: 'الشروط والأحكام'),
-    MoreMenuItem(icon: Icons.gavel_outlined, title: 'إخلاء المسؤولية'),
-    MoreMenuItem(icon: Icons.support_agent_outlined, title: 'الدعم الفني'),
-    MoreMenuItem(icon: Icons.chat_outlined, title: 'الأسئلة الشائعة'),
-    MoreMenuItem(icon: Icons.settings_outlined, title: 'الإعدادات'),
+    MoreMenuItem(
+      icon: Icons.shield_outlined,
+      title: 'سياسة الخصوصية',
+      subtitle: 'الاطلاع على سياسة الخصوصية الخاصة بالمنصة',
+    ),
+    MoreMenuItem(
+      icon: Icons.gavel_outlined,
+      title: 'الشروط والأحكام',
+      subtitle: 'مراجعة الشروط والأحكام المعتمدة',
+    ),
+    MoreMenuItem(
+      icon: Icons.gavel_outlined,
+      title: 'إخلاء المسؤولية',
+      subtitle: 'الاطلاع على بيان إخلاء المسؤولية',
+    ),
+    MoreMenuItem(
+      icon: Icons.support_agent_outlined,
+      title: 'الدعم الفني',
+      subtitle: 'تواصل معنا للاستفسارات والدعم',
+    ),
+    MoreMenuItem(
+      icon: Icons.chat_outlined,
+      title: 'الأسئلة الشائعة',
+      subtitle: 'إجابات سريعة على الأسئلة الأكثر شيوعاً',
+    ),
+    MoreMenuItem(
+      icon: Icons.settings_outlined,
+      title: 'الإعدادات',
+      subtitle: 'إدارة إعدادات حسابك والتطبيق',
+    ),
     MoreMenuItem(
       icon: Icons.logout_rounded,
       title: 'تسجيل الخروج',
+      subtitle: 'تسجيل الخروج من حسابك الحالي',
       isDestructive: true,
     ),
   ];
@@ -53,22 +79,30 @@ class MoreMenu extends StatelessWidget {
     ),
   );
 
-  Widget _buildMenu(BuildContext context) => Card(
-    margin: EdgeInsets.zero,
-    elevation: 1,
-    shadowColor: AppColors.black.withValues(alpha: .1),
-    color: AppColors.white,
-    surfaceTintColor: AppColors.white,
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(AppRadius.r8),
+  Widget _buildMenu(BuildContext context) => Container(
+    decoration: BoxDecoration(
+      color: AppColors.white,
+      borderRadius: BorderRadius.circular(AppRadius.r25),
+      border: Border.all(color: const Color(0xFFE8EDF1)),
+      boxShadow: const [
+        BoxShadow(
+          color: Color.fromRGBO(16, 24, 40, .06),
+          blurRadius: 26,
+          offset: Offset(0, 10),
+        ),
+      ],
     ),
     clipBehavior: Clip.antiAlias,
     child: Column(
       mainAxisSize: MainAxisSize.min,
       children: [
-        for (var index = 0; index < _items.length; index++) ...[
+        for (var index = 0; index < _items.length; index++)
           MoreTile(
             item: _items[index],
+            topPadding: index == 0 ? AppHeight.h16 : AppHeight.h10,
+            bottomPadding: index == _items.length - 1
+                ? AppHeight.h16
+                : AppHeight.h10,
             onTap: index == 0
                 ? () => const ShowProfileRoute().push(context)
                 : index == 1
@@ -87,13 +121,6 @@ class MoreMenu extends StatelessWidget {
                 ? () => _showLogoutDialog(context)
                 : null,
           ),
-          if (index != _items.length - 1)
-            Divider(
-              height: 1,
-              thickness: .7,
-              color: AppColors.lightGrey.withOpacity(0.5),
-            ),
-        ],
       ],
     ),
   );
--- lib/presentation/screens/more/widgets/more_tile.dart
+++ lib/presentation/screens/more/widgets/more_tile.dart
@@ -3,67 +3,88 @@ import 'package:forsan/core/resources/app_colors.dart';
 import 'package:forsan/core/resources/app_fonts.dart';
 import 'package:forsan/core/resources/app_values.dart';
 import 'package:forsan/presentation/screens/more/widgets/more_menu_item.dart';
+import 'package:forsan/presentation/widgets/text/body_title.dart';
+import 'package:icons_plus/icons_plus.dart';
 
 class MoreTile extends StatelessWidget {
-  const MoreTile({super.key, required this.item, this.onTap});
+  const MoreTile({
+    super.key,
+    required this.item,
+    this.onTap,
+    this.topPadding,
+    this.bottomPadding,
+  });
 
   final MoreMenuItem item;
   final VoidCallback? onTap;
+  final double? topPadding;
+  final double? bottomPadding;
 
   @override
   Widget build(BuildContext context) {
-    final foregroundColor = item.isDestructive
-        ? const Color(0xFFFF3158)
-        : AppColors.mainText;
-    final iconBackgroundColor = item.isDestructive
-        ? const Color(0xFFFFF4F6)
-        : AppColors.goldBackGround;
-    final iconColor = item.isDestructive
-        ? const Color(0xFFFF3158)
-        : AppColors.secondaryNormal;
-
-    return ListTile(
-      minTileHeight: item.subtitle == null ? 45 : 45,
-      contentPadding: EdgeInsets.symmetric(horizontal: AppPaddingWidth.p5),
-      leading: Container(
-        width: AppWidth.w45,
-        height: AppWidth.w45,
-        margin:EdgeInsetsDirectional.only(start: AppPaddingWidth.p5),
-        decoration: BoxDecoration(
-          color: iconBackgroundColor,
-          borderRadius: BorderRadius.circular(AppRadius.r12),
-        ),
-        alignment: Alignment.center,
-        child: Icon(item.icon, color: iconColor, size: AppSize.s24),
-      ),
-      title: Text(
-        item.title,
-        textAlign: TextAlign.right,
-        style: TextStyle(
-          color: foregroundColor,
-          fontSize: AppFontSize.s14,
-          fontWeight: AppFontWeight.bold,
-        ),
-      ),
-      subtitle: item.subtitle == null
-          ? null
-          : Text(
-        item.subtitle!,
-        textAlign: TextAlign.right,
-        style: TextStyle(
-          color: AppColors.greyText,
-          fontSize: AppFontSize.s12,
-          fontWeight: AppFontWeight.regular,
-        ),
-      ),
-      trailing: Icon(
-       // item.isDestructive
-            //? Icons.arrow_forward_rounded
-             Icons.chevron_right_rounded,
-        color: AppColors.black,
-        size: AppSize.s24,
-      ),
+    return InkWell(
       onTap: onTap,
+      splashColor: AppColors.none,
+      highlightColor: AppColors.none,
+      child: Padding(
+        padding: EdgeInsetsDirectional.only(
+          start: AppPaddingWidth.p16,
+          end: AppPaddingWidth.p16,
+          top: topPadding ?? 0,
+          bottom: bottomPadding ?? 0,
+        ),
+        child: Row(
+          children: [
+            if (!item.isDestructive) ...[
+              Container(
+                width: AppWidth.w48,
+                height: AppHeight.h48,
+                decoration: BoxDecoration(
+                  color: AppColors.secondaryLight,
+                  borderRadius: BorderRadius.circular(AppRadius.r16),
+                ),
+                child: Icon(
+                  item.icon,
+                  size: AppFontSize.s22,
+                  color: AppColors.secondaryNormal,
+                ),
+              ),
+              SizedBox(width: AppWidth.w14),
+            ],
+            Expanded(
+              child: Column(
+                spacing: AppHeight.h2,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  BodyTitle(
+                    text: item.title,
+                    fontSize: AppFontSize.s17,
+                    fontWeight: AppFontWeight.semiBold,
+                    color: item.isDestructive ? AppColors.red : null,
+                  ),
+                  if (item.subtitle != null)
+                    BodyTitle(
+                      text: item.subtitle,
+                      fontSize: AppFontSize.s13,
+                      fontWeight: AppFontWeight.medium,
+                      color: AppColors.greyText,
+                    ),
+                ],
+              ),
+            ),
+            SizedBox(width: AppWidth.w10),
+            Icon(
+              item.isDestructive
+                  ? Iconsax.logout_outline
+                  : Icons.arrow_forward_ios_rounded,
+              color: item.isDestructive
+                  ? AppColors.red
+                  : const Color(0xFF6E7781),
+              size: AppSize.s20,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
